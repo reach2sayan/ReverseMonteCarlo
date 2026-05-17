cd7990e85812eda75a3f08dd7feeada9316e239e
Sayan <reach2sayan@gmail.com>
2026-05-17 17:44:11 -0400

    improve combined

--- include/RMC/generators/Combined.hpp
+++ include/RMC/generators/Combined.hpp
@@ -18,7 +18,6 @@ public:
     requires(sizeof...(Gs) > 0) && (CMoveGenerator<std::decay_t<Gs>> && ...)
   constexpr explicit CombinedMoveGenerator(Gs &&...gens)
       : generators_(std::forward<Gs>(gens)...) {}
-
   constexpr void generate(IMoveGenerator::Token tok, coords_t &coords,
                           std::span<const std::size_t> indices) {
     std::apply([&](auto &...g) { (g.generate(tok, coords, indices), ...); },
